6099c7a1e7001a23c03f4d13fc366a4e34da7461
raditya <radityaputranto97@yahoo.co.id>
2024-01-04 21:58:29 +0700

    init project

---
+++ server.js
@@ -0,0 +1,41 @@
+import express from 'express';
+
+const app = express();
+
+//set static folder
+app.use(express.static('public'));
+// Parse URL-encoded bodies (as sent by HTML forms)
+app.use(express.urlencoded({ extended: true }));
+// Parse JSON bodies (as sent by API clients)
+app.use(express.json());
+app.disable('etag');
+// Handle GET request to fetch users
+app.get('/users', async (req, res) => {
+    /* const users = [
+        { id: 1, name: 'Jhon Doe' },
+        { id: 2, name: 'Bob Wiwliams' },
+        { id: 1, name: 'Shannon Jacson' },
+    ]; */
+
+    // mimix slow network
+    setTimeout(async () => {
+        const limit = +req.query.limit || 10;
+        const response = await fetch(`https://jsonplaceholder.typicode.com/users?_limit=${limit}`);
+        const users = await response.json();
+
+        res.send(`
+        <h1 class="text-2xl font-bold my-4">
+        Users
+        </h1>
+
+        <ul>
+            ${users.map((user) => `<li>${user.name}</li>`).join(' ')}
+        </ul>
+    `);
+    }, 2000)
+});
+
+// Start the server
+app.listen(3000, () => {
+    console.log('Server listening on port 3000');
+});
